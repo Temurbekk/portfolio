9557b295963e2117d66b61ef1dd18d2524abdd34
Temurbek Sabirov <prodota150@gmail.com>
2019-12-27 14:13:33 -0500

    Updates

--- precache-manifest.1f14371ca0202eecc0fe59bc777b58f2.js
+++ precache-manifest.1f14371ca0202eecc0fe59bc777b58f2.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cfda3f4c4d55c74b27553631ad8aef6f",
+    "revision": "2b4246504c5c998a4c605a23f69e2634",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.7a6bef75.chunk.css"
   },
   {
-    "revision": "0661a2f3f5aa170f2bb0",
+    "revision": "30c715a4d8997863bb06",
     "url": "/portfolio/static/css/main.b99afb53.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.73a2e4d3.chunk.js.LICENSE"
   },
   {
-    "revision": "0661a2f3f5aa170f2bb0",
-    "url": "/portfolio/static/js/main.8612a6c1.chunk.js"
+    "revision": "30c715a4d8997863bb06",
+    "url": "/portfolio/static/js/main.c0a9d4c2.chunk.js"
   },
   {
     "revision": "8575fab3f3395c90d845",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.529a3284451e32950ec746a377c039ef.js"
+  "/portfolio/precache-manifest.1f14371ca0202eecc0fe59bc777b58f2.js"
 );
 
 self.addEventListener('message', (event) => {
