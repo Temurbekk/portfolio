e11fd6b5f08e51cd72549cbb1d83f8fdc3063eac
Temurbek Sabirov <prodota150@gmail.com>
2019-12-27 13:04:14 -0500

    Updates

---
+++ precache-manifest.529a3284451e32950ec746a377c039ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfda3f4c4d55c74b27553631ad8aef6f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2fe4506df749a801dbdb",
+    "url": "/portfolio/static/css/2.7a6bef75.chunk.css"
+  },
+  {
+    "revision": "0661a2f3f5aa170f2bb0",
+    "url": "/portfolio/static/css/main.b99afb53.chunk.css"
+  },
+  {
+    "revision": "2fe4506df749a801dbdb",
+    "url": "/portfolio/static/js/2.73a2e4d3.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/portfolio/static/js/2.73a2e4d3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0661a2f3f5aa170f2bb0",
+    "url": "/portfolio/static/js/main.8612a6c1.chunk.js"
+  },
+  {
+    "revision": "8575fab3f3395c90d845",
+    "url": "/portfolio/static/js/runtime-main.680679b4.js"
+  },
+  {
+    "revision": "d5bf13c671e7cfd3c3487b756fa2ab2c",
+    "url": "/portfolio/static/media/Profile_pic.d5bf13c6.png"
+  },
+  {
+    "revision": "df137b674d25ed2ed075ebb99188409b",
+    "url": "/portfolio/static/media/hallway-pic.df137b67.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.329d2ebebdd3d90a3df526ccd043c458.js"
+  "/portfolio/precache-manifest.529a3284451e32950ec746a377c039ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
