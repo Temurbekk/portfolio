fee53df8f988bc531c372bd51bc3c8e349c40a9e
Temurbek Sabirov <prodota150@gmail.com>
2020-05-27 23:20:49 -0400

    Updates

---
+++ precache-manifest.59efb9f1254537e438627e606d1e3b74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da158d14b79ce90851a44dac43c85130",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dc552f1fd660880c1535",
+    "url": "/portfolio/static/css/main.4d822c19.chunk.css"
+  },
+  {
+    "revision": "a94eec8f69f35dd308bc",
+    "url": "/portfolio/static/js/2.95805b0d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.95805b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc552f1fd660880c1535",
+    "url": "/portfolio/static/js/main.246cffff.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  }
+]);
