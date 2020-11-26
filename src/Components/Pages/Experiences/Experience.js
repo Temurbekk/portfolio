3b5abfc9ad6dbe3bfea7eeb90286a8c3a1f196ce
Temurbek Sabirov <prodota150@gmail.com>
2020-11-26 15:37:32 -0500

    Change experiences style

--- src/Components/Pages/Experiences/Experience.js
+++ src/Components/Pages/Experiences/Experience.js
@@ -13,22 +13,22 @@ const Experience = ({
   return (
     <Fade bottom>
       <div className="card">
-        <div className="card-image">
-          <figure className="image is-4by3">
-            <img src={imageSrc} alt={title} />
-          </figure>
-        </div>
         <div className="card-content">
           <div className="media">
-            <div className="media-content">
-              <p className="title is-4">
-                <a href={url}>{title}</a>
-              </p>
-              <p className="subtitle is-6">Job Title: {job}</p>
-              <p className="subtitle is-6">Technologies: {technologies}</p>
+            <div class="media-left">
+              <figure class="image is-128x128">
+                <img src={imageSrc} alt={title} />
+              </figure>
             </div>
           </div>
-
+          <div className="media-content">
+            <p className="title is-4">
+              <a href={url}>{title}</a>
+            </p>
+            <p className="subtitle is-6">Job Title: {job}</p>
+            <p className="subtitle is-6">Technologies: {technologies}</p>
+          </div>
+          <div className="p-3"></div>
           <div className="content">
             {description}
             <br />
